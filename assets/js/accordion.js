b03a92700db4fc8e21fdbefac34a09901f7893d0
giulianoCriscuoli <giulianodias3@hotmail.com>
2025-02-18 16:28:05 -0300

    Finalização da estilização do accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,5 +1,13 @@
-const accordion = document.querySelector(".accordion");
+const accordions = document.querySelectorAll(".accordion-flex");
 
-accordion.addEventListener('click', () => {
-    this.classList.add('ativo')
+accordions.forEach(accordion => {
+    accordion.addEventListener("click", () => {
+        accordion.classList.toggle("ativo");
+
+        const conteudo = accordion.querySelector(".accordion-conteudo");
+
+        if (conteudo) {
+            conteudo.style.display = conteudo.style.display === "inline-block" ? "none" : "inline-block";
+        }
+    });
 });
